b999a85a639b78dffe3108c371a8763cb8629f2e
Mike Mazdra <mazdram@health.missouri.edu>
2014-09-05 12:01:16 -0500

    Added breakout to homepage template with img background

--- content/themes/bones/page-custom.php
+++ content/themes/bones/page-custom.php
@@ -1,6 +1,6 @@
 <?php
 /*
- Template Name: Custom Page Example
+ Template Name: Homepage
  *
  * This is your custom page template. You can create as many of these as you need.
  * Simply name is "page-whatever.php" and in add the "Template Name" title at the
@@ -15,6 +15,9 @@
 
 <?php get_header(); ?>
 
+<?php 
+    echo do_shortcode("[metaslider id=36]"); 
+?>
 			<div id="content">
 
 				<div id="inner-content" class="wrap cf">
@@ -28,11 +31,6 @@
 								<header class="article-header">
 
 									<h1 class="page-title"><?php the_title(); ?></h1>
-<!--
-									<p class="byline vcard">
-										<?php printf( __( 'Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span>', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
-									</p>
--->
 
 								</header>
 
@@ -40,19 +38,7 @@
 									<?php
 										// the content (pretty self explanatory huh)
 										the_content();
-
-										/*
-										 * Link Pages is used in case you have posts that are set to break into
-										 * multiple pages. You can remove this if you don't plan on doing that.
-										 *
-										 * Also, breaking content up into multiple pages is a horrible experience,
-										 * so don't do it. While there are SOME edge cases where this is useful, it's
-										 * mostly used for people to get more ad views. It's up to you but if you want
-										 * to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
-										 *
-										 * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
-										 *
-										*/
+				
 										wp_link_pages( array(
 											'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
 											'after'       => '</div>',
@@ -61,7 +47,11 @@
 										) );
 									?>
 								</section>
-
+								
+								
+							
+								
+								
 
 								<footer class="article-footer">
 
@@ -91,10 +81,14 @@
 
 						</div>
 
-						
 
 				</div>
-
+				
+						<section class ="full-breakout-img">
+								<h2>Discovering a Needle in a Haystack for Muscular Dystrophy Patients</h2>
+								<p>Research on significant genetic sequence could lead to treatments for deadly hereditary disease</p>
+								<button class="blue-btn">Read More</button>
+						</section>
 			</div>
 
 
