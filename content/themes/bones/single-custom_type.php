0265932a20446cde01207ce1db49af88d9ef1453
Mike Mazdra <mazdram@health.missouri.edu>
2014-09-05 12:02:14 -0500

    Defined button properties and added breakout styling

--- content/themes/bones/single-custom_type.php
+++ content/themes/bones/single-custom_type.php
@@ -20,7 +20,9 @@
 			<div id="content">
 
 				<div id="inner-content" class="wrap cf">
-
+					<?php 
+    echo do_shortcode("[metaslider id=36]"); 
+?>
 						<div id="main" class="m-all t-3of3 d-7of7 cf" role="main">
 
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
